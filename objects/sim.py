7fd7181f5723f20c3b5ce5c85a3f60cd5f270aec
Alkım Deniz Altıntaş <altalk.23@robcol.k12.tr>
2020-06-23 22:21:51 +0300

    draggable stage

--- objects/sim.py
+++ objects/sim.py
@@ -7,28 +7,42 @@ from visual import colors
 class Sim(Object):
     def __init__(self, stage):
         self.stage = stage
+        self.pos = (0, 0)
         self.rectangle = pygame.rect.Rect(
-            randint(50, self.stage.width-50),
-            randint(50, self.stage.height-50),
+            self.pos[0],
+            self.pos[1],
             20,
             20
         )
         self.selected = False
 
-    def on_mouse_up(self, event):
+    def on_mouse_down(self, event):
         if self.rectangle.collidepoint(event.pos):
+            self.stage.dragging = False
+
+    def on_mouse_up(self, event):
+        if self.rectangle.collidepoint(event.pos) and not self.stage.dragging:
             self.selected = not self.selected
         else:
             self.selected = False
 
     def loop(self):
+        self.pos = self.stage.rel_pos(0, 0)
+        self.rectangle = pygame.rect.Rect(
+            self.pos[0],
+            self.pos[1],
+            20,
+            20
+        )
         if self.selected:
             pygame.draw.rect(self.stage.screen, colors.bright_green, self.rectangle)
         else:
             pygame.draw.rect(self.stage.screen, colors.bright_red, self.rectangle)
 
     def get_data(self):
+        self.speed = 1
+        self.rotation = 0
         pass
 
-    def add_data(self):
+    def set_data(self):
         pass
--- visual/stage.py
+++ visual/stage.py
@@ -9,24 +9,36 @@ class Stage:
         pygame.init()
         self.objects = []
         self.objects.append(Framerate(self))
+        self.dragging = False
+        self.dx = 0
+        self.dy = 0
+        self.drag = (0,0)
+
 
     def set_font(self, font, size):
         self.font = pygame.font.Font(font, size)
 
+
     def add_sim(self):
         self.objects.append(Sim(self))
+
 
     def set_framerate(self, framerate):
         self.clock = pygame.time.Clock()
         self.framerate = framerate
+
 
     def set_window(self, width, height):
         self.width = width
         self.height = height
         self.screen = pygame.display.set_mode((width, height))
 
+    def rel_pos(self, x, y):
+        return (x + self.dx, y + self.dy)
+
     def start(self):
         self.running = True
+
         while self.running:
             self.screen.fill(colors.white)
             self.deltatime = self.clock.tick(self.framerate)
@@ -36,6 +48,10 @@ class Stage:
                     self.running = False
 
                 elif event.type == pygame.MOUSEBUTTONDOWN:
+                    self.dragging = True
+
+                    self.drag = event.pos
+
                     if event.button == 1:
                         for object in self.objects:
                             object.on_mouse_down(event)
@@ -44,6 +60,17 @@ class Stage:
                     if event.button == 1:
                         for object in self.objects:
                             object.on_mouse_up(event)
+
+                    self.dragging = False
+
+                elif event.type == pygame.MOUSEMOTION:
+                    if self.dragging:
+                        x = event.pos[0] - self.drag[0]
+                        y = event.pos[1] - self.drag[1]
+                        self.dx += x
+                        self.dy += y
+                        self.drag = event.pos
+
 
             for object in self.objects:
                 object.loop()
