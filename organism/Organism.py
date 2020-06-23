ce56d5e7ba407516878bbd9a34fa2b2da1998a60
birkiy.Ku.PC <umutberkayaltintas@gmail.com>
2020-06-23 22:20:32 +0300

    simGene init

---
+++ organism/Organism.py
@@ -0,0 +1,17 @@
+class simGenome():
+    def __init__(self, idx):
+        self.idx = idx
+
+        self.nodes = {}
+        self.edges = {}
+
+        self.fitness = None
+
+
+
+
+class simGene():
+    def __init__(self, key):
+        self.key = key
+
+    
